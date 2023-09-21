5528e2405a9139b3080d891847b94cac47c7a355
G2TheG <istvanrusznyak2001@gmail.com>
2023-09-21 09:42:19 +0200

    3 gyerek van-e

--- 03.py
+++ 03.py
@@ -1,10 +1,13 @@
 #Jövedelemszámítási feladat
 print("Jövedelemszámítás\n")
 kor = int(input("Hány éves vagy?"))
-gyerek = input("Van 3 gyereked és Nő vagy ()igen/nem?")
+gyerek = input("Van 3 gyereked és Nő vagy (igen/nem?)")
 brutto = int(input("Mennyi a brutto jövedelmed?"))
 if kor <= 25 or gyerek in["igen", "Igen", "i", "I"]:
-    szja = 0
+    if brutto > 500000:
+        szja = (brutto-500000) * 0.15
+    else:
+        szja = 0
 else:
     szja = brutto * 0.15
 
