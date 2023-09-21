e436c515bf5b68c19182ce8bc247b3cc865eec75
G2TheG <istvanrusznyak2001@gmail.com>
2023-09-21 09:55:36 +0200

    Kiegészítés

--- 03.py
+++ 03.py
@@ -1,7 +1,8 @@
 #Jövedelemszámítási feladat
 print("Jövedelemszámítás\n")
 kor = int(input("Hány éves vagy?"))
-gyerek = input("Van 3 gyereked és Nő vagy (igen/nem?)")
+if kor > 25:
+    gyerek = input("Van 3 gyereked és Nő vagy (igen/nem?)")
 brutto = int(input("Mennyi a brutto jövedelmed?"))
 if kor <= 25 or gyerek in["igen", "Igen", "i", "I"]:
     if brutto > 500000:
@@ -11,4 +12,4 @@ if kor <= 25 or gyerek in["igen", "Igen", "i", "I"]:
 else:
     szja = brutto * 0.15
 
-print(f"SZJA: {szja}")
+print("SZJA:".ljust(20,"-"), str(int(szja)).rjust(10,"-"),sep="")
